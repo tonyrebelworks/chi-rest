156b06bfa2edfc69a5e650d322146ad5b21195e0
Pieter Lelaona <pieter@rebelworks.co>
2020-03-20 04:10:28 +0700

    remove unecessary files and folder

--- viewmodel/member_vm.go
+++
@@ -1,9 +0,0 @@
-package viewmodel
-
-// MemberVM ...
-type MemberVM struct {
-	Fname string `json:"fname"`
-	Lname string `json:"lname"`
-	Email string `json:"email"`
-	Phone string `json:"phone"`
-}
